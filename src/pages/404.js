d30c659e4d4b0ddfc524f1460f8e5be9f472b76d
SpyECho <bakchaloni@gmail.com>
2020-12-20 18:30:27 +0100

    working on it tag

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react"
-import i404 from "../images/404.webp"
+import img from "../images/404.webp"
 import styles from "../css/workin.module.css"
 export default class error extends Component {
   render() {
     return (
       <div>
-        <img src={i404} style={styles.full} alt="" />
+        <img src={img} className={styles.full} alt="" />
       </div>
     )
   }
