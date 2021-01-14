e5a5b9a071aa58d52b36da2726d58df82b4aa8d6
SpyECho <bakchaloni@gmail.com>
2021-01-14 16:29:46 +0100

    v1 of about section in home page

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,13 +1,33 @@
 import React from "react"
 import Title from "../Title"
 import styled from "styled-components"
-
+import photo from "../../images/profile.jpg"
+import styles from "../../css/about.module.css"
 const About = ({ className }) => {
   return (
-    <div className={className}>
+    <section className={styles.about}>
       <Title titel="<About" subtitel="Me/>" />
-      Hello from About section
-    </div>
+      <div className={styles.aboutCenter}>
+        <article cle className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={photo} alt="Sohaib Arbi Bakcha" />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <p>
+            My name is Sohaib Arbi Bakcha, I have{" "}
+            {new Date().getFullYear() - 1997} Years Old, I live in Morocco
+          </p>
+          <p>
+            I started learning Programing in 2017, If you wanna know more about
+            me press the button blow
+          </p>
+          <button type="button" className="btn-primary">
+            Know More
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 
