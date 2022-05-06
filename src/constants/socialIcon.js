ae16fcad118c408ead05efd568c2c2250dfdb44b
BAKCHA Sohaib Arbi <51241996+SohaibArbiBakcha@users.noreply.github.com>
2022-05-06 09:43:21 +0000

    modify social links

--- src/constants/socialIcon.js
+++ src/constants/socialIcon.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { FaFacebook, FaInstagram, FaGithub } from "react-icons/fa"
+import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa"
 
 export default [
   {
@@ -7,8 +7,8 @@ export default [
     url: "https://github.com/SohaibArbiBakcha",
   },
   {
-    icon: <FaFacebook />,
-    url: "https://www.facebook.com/sohaib.spy/",
+    icon: <FaLinkedin />,
+    url: "https://www.linkedin.com/in/sohaibarbibakcha",
   },
   {
     icon: <FaInstagram />,
